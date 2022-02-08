065460aaae23e78468e77ed467793878af8fb24c
Jordan <Jordan19731030@gmail.com>
2022-02-08 10:16:32 +0800

    init

---
+++ scripts/feeTo.ts
@@ -0,0 +1,35 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+
+const feeReceiver  = "0xA8898aFC67Bb36AC63Dc7F0Ae4dD437E9065B924";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const PancakeFactory = await ethers.getContractFactory("PancakeFactory");
+  const factory = await PancakeFactory.attach("0xcd8620889c1dA22ED228e6C00182177f9dAd16b7");
+
+  await factory.deployed();
+
+//   await factory.setFeeTo(feeReceiver);
+  //Shibuya: 0x0558677b74f6dc11ee476ed79ce993579e32b1ae87e818f42c4f9d829c1db12a
+  //Shiden: 0x0558677b74f6dc11ee476ed79ce993579e32b1ae87e818f42c4f9d829c1db12a
+  console.log("factory feeTo:", await factory.feeTo());
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
